85bd05386af9cf26b60d3bebec3205af61b9f0b3
congdoan176 <doanncd00530@fpt.edu.vn>
2018-06-26 19:45:20 +0700

    Bai thi mon C#

---
+++ ThiCShap/Product.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.NetworkInformation;
+
+namespace ThiCShap
+{
+    public class Product
+    {
+        private string id;
+        private string name;
+        private int price;
+
+        public Product(string id, string name, int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+        }
+
+        public string Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Price
+        {
+            get => price;
+            set => price = value;
+        }
+    }
+}
---
+++ ThiCShap/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+
+namespace ThiCShap
+{
+    class Program
+    {
+        private static List<Product> list = new List<Product>();
+        
+        static void Main(string[] args)
+        {
+            GenerateMenu();
+        }
+
+        public static void GenerateMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("----Menu----");
+                Console.WriteLine("1. Add product records.");
+                Console.WriteLine("2. Display product records.");
+                Console.WriteLine("3. Delete product by Id.");
+                Console.WriteLine("4. Exit.");
+            
+                int choice = Int32.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        AddProduct();
+                        break;
+                    case 2:
+                        
+                        DisplayProduct();
+                        break;
+                    case 3:
+
+                        Console.WriteLine("Nhap id can xoa.");
+                        var id = Console.ReadLine();
+                        DeleteProduct(id);
+                        break;
+                    case 4:
+                        Console.WriteLine("Exit.");
+                        Environment.Exit(1);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+        }
+
+        public static void AddProduct()
+        {
+            Console.WriteLine("Vui long nhap ID: ");
+            string id = Console.ReadLine();
+            Console.WriteLine("Vui long nhap Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("vui long nhap Price: ");
+            int price = Int32.Parse(Console.ReadLine());
+            
+            Product product = new Product(id, name, price);
+            list.Add(product);
+
+        }
+
+        public static void DisplayProduct()
+        {
+            Console.WriteLine(string.Format("{0, 10} | {1 , 20} | {2, 20}","Product ID", "Product Name","Price"));
+            foreach (var product in list)
+            {
+                Console.WriteLine(string.Format("{0,10} | {1, 20} | {2, 20}",product.Id,product.Name,"$" +product.Price));
+            }
+        }
+
+        public static void DeleteProduct(string id)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Id == id)
+                {
+                    list.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
